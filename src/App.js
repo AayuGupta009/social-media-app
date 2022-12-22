3abbc250504536a7ed501d7766a399f58f6a56de
Aayush Gupta <aayushgupta.dev@gmail.com>
2022-12-22 18:01:32 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-import './App.css';
+import styles from  './App.module.css';
 
 function App() {
+  let name;
+  let isGreen = true;
   return (
-    <div className="App">
-      <User name="pedro" age={22} email="aayush@email.com" />
-      <br />
-      <Job salary={ 6000 } position="Junior SDE" company="Apple" />
-      <Job salary={ 9000 } position="Senior SDE" company="Netflix" />
-      <Job salary={ 10000 } position="Product Manager" company="Netflix" />
+    <div className={styles.App}>
+      <div className={styles.name} style={{ color : isGreen ? 'blue' : 'red'}}>{name ? <h1>{ name }</h1> : <h1>"Good Night"</h1> }</div>
     </div>
   );
 }
-
-const User = (props) => {
-  return (
-    <div className='user'>
-      <h1>{ props.name }</h1>
-      <h2>{ props.age }</h2>
-      <h3>{ props.email }</h3>
-    </div>
-  )
-}
-
-const Job = (props) => {
-  return (
-    <div className='job'>
-      <h1>{ props.salary }</h1>
-      <h1>{ props.company }</h1>
-      <h1>{ props.position }</h1>
-    </div>
-  )
-}
-
 export default App;
