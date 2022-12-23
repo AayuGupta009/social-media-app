806c120124ae98868d05cfafa92821f4e8756306
Aayush Gupta <aayushgupta.dev@gmail.com>
2022-12-23 12:06:32 +0530

    basic state and useState hook

--- src/App.js
+++ src/App.js
@@ -1,62 +1,15 @@
-import styles from './App.module.css';
-import {
-    Planet
-} from './Planet'
+import styles from  './App.module.css';
+import { useState } from 'react'
 
 function App() {
-    let users = [{
-        name: "Aayush",
-        age: 12
-    }, {
-        name: "Shubham",
-        age: 21
-    }, {
-        name: "Jaiki",
-        age: 12
-    }, {
-        name: "Akhand",
-        age: 37
-    }]
-    let planets = [{
-            name: "Mars",
-            isGasPlanet: false
-        },
-        {
-            name: "Earth",
-            isGasPlanet: false
-        },
-        {
-            name: "Jupiter",
-            isGasPlanet: true
-        },
-        {
-            name: "Venus",
-            isGasPlanet: false
-        },
-        {
-            name: "Neptune",
-            isGasPlanet: true
-        },
-        {
-            name: "Uranus",
-            isGasPlanet: true
-        },
-    ];
+  const [age , setAge] = useState(0)
   return (
-    <
-      div className={
-            styles.App
-        } > {
-            planets.map((planet, key) => planet.isGasPlanet && < Planet planet = {
-                    planet.name
-                }
-                key = {
-                    key
-                }
-                />
-            )
-      } </div>
-    );
+    <div className={styles.App}>
+      {age}
+      <br />
+      <button onClick={() => setAge( age + 1)}>Click me!</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
