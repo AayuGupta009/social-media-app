ac3de519f9035f05aeb3d4446305b66a6d895f03
Aayush Gupta <aayushgupta.dev@gmail.com>
2022-12-23 12:45:08 +0530

    Counter App using State

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import styles from  './App.module.css';
 import { useState } from 'react'
 
 function App() {
-  const [changeColor, setChangeColor] = useState("green")
-  const alterColor = () => setChangeColor(changeColor === 'green' ? "red" : "green")
+  const [count, setCount] = useState(0)
+  
+  const increaseCount = () => setCount(count + 1)
+  const decreaseCount = () => setCount(count - 1)
+  const setToZero = () => setCount(0)
+  
   return (
     <div className={styles.App}>
-      <button onClick={alterColor}>Change text Color</button>
+      <button onClick={ increaseCount }>Increase</button>
+      <button onClick={ decreaseCount }>Decrease</button>
+      <button onClick={ setToZero }>Set to Zero</button>
+
       <br />
-      <h1 style={{color : changeColor}}>Hi</h1>
+      {count}
     </div>
   );
 }
